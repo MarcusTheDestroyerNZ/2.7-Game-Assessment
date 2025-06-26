f8c24f4296829fe1dcbfcd71d2dc554ddc5c0d69
MarcusTheDestroyerNZ <120174246+MarcusTheDestroyerNZ@users.noreply.github.com>
2025-06-26 12:55:08 +1200

    Moved all files froward and got rid of the testing folders

--- Map Testing/Map1/forestTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="forestTileset" tilewidth="64" tileheight="64" tilecount="198" columns="22">
- <image source="forest.jpg" width="1408" height="576"/>
-</tileset>
--- Testing/Test1.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-img = Image.open("Testing/forest.jpg")
-img = img.resize((img.width * 4, img.height * 4), Image.NEAREST)
-img.save("forest_resized.jpg")
